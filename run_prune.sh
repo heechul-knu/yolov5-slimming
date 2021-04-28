45a381606d8d902c37d78b75c3a6cec2bb907da6
heechul-knu <heechul@knu.ac.kr>
2021-04-28 18:23:41 +0900

    commit

---
+++ run_prune.sh
@@ -0,0 +1,1 @@
+python3 prune.py --data recycle.yaml --cfg yolov5m.yaml --weights ./weights/yolov5_m_0324.pt --batch-size 16
---
+++ run_train.sh
@@ -0,0 +1,1 @@
+python3 train.py --data recycle.yaml --cfg yolov5m.yaml --weights '' --batch-size 16
